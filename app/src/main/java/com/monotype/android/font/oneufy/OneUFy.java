b9cd148bf9e090107f5016afdefc0b006ce08bb6
MRX7014 <safen894@gmail.com>
2026-07-06 11:22:41 +0300

    initial commit

---
+++ app/src/main/java/com/monotype/android/font/oneufy/OneUFy.java
@@ -0,0 +1,27 @@
+package com.monotype.android.font.oneufy;
+
+import android.content.Context;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class OneUFu implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.android.settings")) return;
+        XposedHelpers.findAndHookMethod(
+                "com.samsung.android.settings.display.SecDisplayUtils",
+                lpparam.classLoader,
+                "isInvalidFont",
+                Context.class,
+                String.class,
+                new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                        return false;
+                    }
+                }
+        );
+    }
+}
